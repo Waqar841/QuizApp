5ffa9baad45adb0cb6d4623e997ef367fce67d51
Waqar841 <157989041+Waqar841@users.noreply.github.com>
2024-03-28 15:34:10 -0400

    Update script.js

--- QuizApp/QuizApp_me/script.js
+++ QuizApp/QuizApp_me/script.js
@@ -1,8 +1,9 @@
 
 const quizPart=document.querySelector('.quiz');
-const strtBtn=document.querySelector('.start-btn');
-
-console.log(quizPart);
+const strtBtn = document.querySelector('.start-btn');
+const allAnswers = document.querySelectorAll('.answers');
+const answer_section = document.querySelector('.answer-section');
+const correctAnswer = 'Charles Babbage';
 
 window.onload = function () {
     quizPart.style.display='none';
@@ -10,6 +11,17 @@ window.onload = function () {
         strtBtn.style.display='none';
         quizPart.style.display='block';
     });
+
+    // it gets item on which user clicked
+    allAnswers.forEach(function (selectedDiv) { 
+        selectedDiv.addEventListener('click', function () {
+            answer_section.classList.add('unclickable');
+            var urAnswer = selectedDiv.querySelector('p').innerText;
+
+            urAnswer === correctAnswer ? selectedDiv.classList.add('right-answer')
+                : selectedDiv.classList.add('wrong-answer');
+        })
+    })    
 };
 
 
