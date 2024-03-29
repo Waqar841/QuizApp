595ff01b2127a320deca27dbc82de1c75cc7b5f4
Waqar841 <157989041+Waqar841@users.noreply.github.com>
2024-03-29 16:05:55 -0400

    Update script.js

--- QuizApp/script.js
+++ QuizApp/script.js
@@ -1,178 +1,48 @@
-//References
-let timeLeft = document.querySelector(".time-left");
-let quizContainer = document.getElementById("container");
-let nextBtn = document.getElementById("next-button");
-let countOfQuestion = document.querySelector(".number-of-question");
-let displayContainer = document.getElementById("display-container");
-let scoreContainer = document.querySelector(".score-container");
-let restart = document.getElementById("restart");
-let userScore = document.getElementById("user-score");
-let startScreen = document.querySelector(".start-screen");
-let startButton = document.getElementById("start-button");
-let questionCount;
-let scoreCount = 0;
-let count = 11;
-let countdown;
 
-//Questions and Options array
+const quizPart=document.querySelector('.quiz');
+const strtBtn = document.querySelector('.start-btn');
+const allAnswers = document.querySelectorAll('.answers');
+const answer_section = document.querySelector('.answer-section');
+const Question_2 = document.querySelector('.q-question');
+let correctAnswer = 'Charles Babbage';
+const nextBtn = document.querySelector('.btn-div');
+const Ans1 = document.querySelector('.Ans-1');
+const Ans2 = document.querySelector('.Ans-2');
+const Ans3 = document.querySelector('.Ans-3');
+const Ans4 = document.querySelector('.Ans-4');
+let count = 0;
 
-const quizArray = [
-  {
-    id: "0",
-    question: "Which is the most widely spoken language in the world?",
-    options: ["Spanish", "Mandarin", "English", "German"],
-    correct: "Mandarin",
-  },
-  {
-    id: "1",
-    question: "Which is the only continent in the world without a desert?",
-    options: ["North America", "Asia", "Africa", "Europe"],
-    correct: "Europe",
-  },
-  {
-    id: "2",
-    question: "Who invented Computer?",
-    options: ["Charles Babbage", "Henry Luce", "Henry Babbage", "Charles Luce"],
-    correct: "Charles Babbage",
-  },
-];
 
-//Restart Quiz
-restart.addEventListener("click", () => {
-  initial();
-  displayContainer.classList.remove("hide");
-  scoreContainer.classList.add("hide");
-});
-
-//Next Button
-nextBtn.addEventListener(
-  "click",
-  (displayNext = () => {
-    //increment questionCount
-    questionCount += 1;
-    //if last question
-    if (questionCount == quizArray.length) {
-      //hide question container and display score
-      displayContainer.classList.add("hide");
-      scoreContainer.classList.remove("hide");
-      //user score
-      userScore.innerHTML =
-        "Your score is " + scoreCount + " out of " + questionCount;
-    } else {
-      //display questionCount
-      countOfQuestion.innerHTML =
-        questionCount + 1 + " of " + quizArray.length + " Question";
-      //display quiz
-      quizDisplay(questionCount);
-      count = 11;
-      clearInterval(countdown);
-      timerDisplay();
-    }
-  })
-);
-
-//Timer
-const timerDisplay = () => {
-  countdown = setInterval(() => {
-    // count--;
-    timeLeft.innerHTML = `${count}s`;
-    if (count == 0) {
-      clearInterval(countdown);
-      displayNext();
-    }
-  }, 1000);
-};
-
-//Display quiz
-const quizDisplay = (questionCount) => {
-  let quizCards = document.querySelectorAll(".container-mid");
-  //Hide other cards
-  quizCards.forEach((card) => {
-    card.classList.add("hide");
-  });
-  //display current question card
-  quizCards[questionCount].classList.remove("hide");
-};
-
-//Quiz Creation
-function quizCreator() {
-  //randomly sort questions
-  quizArray.sort(() => Math.random() - 0.5);
-  //generate quiz
-  for (let i of quizArray) {
-    //randomly sort options
-    i.options.sort(() => Math.random() - 0.5);
-    //quiz card creation
-    let div = document.createElement("div");
-    div.classList.add("container-mid", "hide");
-    //question number
-    countOfQuestion.innerHTML = 1 + " of " + quizArray.length + " Question";
-    //question
-    let question_DIV = document.createElement("p");
-    question_DIV.classList.add("question");
-    question_DIV.innerHTML = i.question;
-    div.appendChild(question_DIV);
-    //options
-    div.innerHTML += `
-    <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
-     <button class="option-div" onclick="checker(this)">${i.options[1]}</button>
-      <button class="option-div" onclick="checker(this)">${i.options[2]}</button>
-       <button class="option-div" onclick="checker(this)">${i.options[3]}</button>
-    `;
-    quizContainer.appendChild(div);
-  }
-}
-
-//Checker Function to check if option is correct or not
-function checker(userOption) {
-  let userSolution = userOption.innerText;
-  let question =
-    document.getElementsByClassName("container-mid")[questionCount];
-  let options = question.querySelectorAll(".option-div");
-
-  //if user clicked answer == correct option stored in object
-  if (userSolution === quizArray[questionCount].correct) {
-    userOption.classList.add("correct");
-    scoreCount++;
-  } else {
-    userOption.classList.add("incorrect");
-    //For marking the correct option
-    options.forEach((element) => {
-      if (element.innerText == quizArray[questionCount].correct) {
-        element.classList.add("correct");
-      }
+window.onload = function () {
+    quizPart.style.display='none';
+    strtBtn.addEventListener('click',function(){
+        strtBtn.style.display='none';
+        quizPart.style.display='block';
     });
-  }
 
-  //clear interval(stop timer)
-  clearInterval(countdown);
-  //disable all options
-  options.forEach((element) => {
-    element.disabled = true;
-  });
-}
+    // It gets that item on which user has clicked
+    allAnswers.forEach(function (selectedDiv) { 
+        selectedDiv.addEventListener('click', function () {
+            answer_section.classList.add('unclickable');
+            var urAnswer = selectedDiv.querySelector('p').innerText;
 
-//initial setup
-function initial() {
-  quizContainer.innerHTML = "";
-  questionCount = 0;
-  scoreCount = 0;
-  count = 11;
-  clearInterval(countdown);
-  timerDisplay();
-  quizCreator();
-  quizDisplay(questionCount);
-}
+            urAnswer === correctAnswer ? selectedDiv.classList.add('right-answer')
+                : selectedDiv.classList.add('wrong-answer');
+        })
+    })
 
-//when user click on start button
-startButton.addEventListener("click", () => {
-  startScreen.classList.add("hide");
-  displayContainer.classList.remove("hide");
-  initial();
-});
+    nextBtn.addEventListener('click', function () { 
+        Ans1.innerText = count === 0 ? 'Mandarin' : 'Africa';
+        Ans2.innerText = count === 0 ? 'Spanish' : 'Europe';
+        Ans3.innerText = count === 0 ? 'English' : 'Asia';
+        Ans4.innerText = count === 0 ? 'German' : 'North America';
+        count === 0 ? Question_2.innerText = 'Which is the most widely spoken language in the world?' :
+            Question_2.innerText = 'Which is the only continent in the world without a desert?'
 
-//hide quiz and display start screen
-window.onload = () => {
-  startScreen.classList.remove("hide");
-  displayContainer.classList.add("hide");
+        count === 0 ? correctAnswer = 'English' : correctAnswer = 'Europe';
+        count = 1;
+
+        answer_section.classList.remove('unclickable');
+        
+    })
 };
